bf4a038d9fbf89f6089e5b4402a224560acd8ace
panud007 <panud007@users.noreply.github.com>
2026-06-15 11:52:27 +0700

    Allow host pcc.ngntd.my.id in Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  server: {
+    allowedHosts: ['pcc.ngntd.my.id']
+  },
+  preview: {
+    allowedHosts: ['pcc.ngntd.my.id']
+  }
 })
 
